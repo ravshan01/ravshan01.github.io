67ef6508f77c73fffd3d9fb3be9a1e0ed65e0342
ravshan01 <51333533+ravshan01@users.noreply.github.com>
2020-05-08 22:39:33 +0500

    add skills page

---
+++ skills.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ru_RU">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+    <title>Document</title>
+    <meta name="description" content="Some text" />
+    <meta name="Author" content="_blank, Selimov Ravshan" />
+    <meta name="Copyright" content="Selimov Ravshan" />
+
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/media.css">
+    <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/main.js"></script>
+  </head>
+  <body>
+
+  </body>
+</html>
