641bf9c93c0a3854aa29142c1cc66b2d064f7329
ravshan01 <51333533+ravshan01@users.noreply.github.com>
2020-10-17 18:41:30 +0500

    build project

--- gulpfile.js
+++
@@ -1,6 +0,0 @@
-const build = require('./gulp/build/build.js');
-const server = require('./gulp/server/server.js');
-
-
-module.exports.build = build;
-module.exports.server = server;
--- src/assets/js/about.js
+++
@@ -1,8 +0,0 @@
-createAnimateText( document.querySelector('h1') );
-
-
-const slider = createSlider(document.querySelector('.slider'), {
-	controls: false,
-	autoSlide: true,
-	duration: 3500
-})
