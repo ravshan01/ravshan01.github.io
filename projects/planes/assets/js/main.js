67bcadd765c9454896f6fc0f1a837a54a6aae217
ravshan01 <51333533+ravshan01@users.noreply.github.com>
2020-10-22 20:41:49 +0500

    new build

--- projects/planes/assets/js/main.js
+++ projects/planes/assets/js/main.js
@@ -1,1 +1,1 @@
-window.addEventListener("load",(()=>{document.querySelector(".preloader-wrap").style.display="none",new App({distinctPlanesCount:5,colors:["black","blue","green","pink","orange"],defaultSpeed:1.5,defaultSpawnTime:.75,defaultRoundTime:30}).init()}));
+window.addEventListener("load",(()=>{document.querySelector(".preloader-wrap").style.display="none";new App({distinctPlanesCount:5,colors:["black","blue","green","pink","orange"],settings:{roundTime:{min:15,max:120,default:30},spawnTime:{min:.25,max:1.5,default:.75},speed:{min:.5,max:2,default:1.5}}}).init()}));
